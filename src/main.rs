19a77bb77fdea5592933ce080eef2f1d0431332e
Anthony <carvekidsak@gmail.com>
2025-11-29 22:38:15 -0900

    Finished !!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod utils;
 
-use std::path::PathBuf;
-use crate::utils::*;
+use std::{os::windows::process::CommandExt, path::{Path, PathBuf}, process::Command};
+
+use windows::Win32::System::Threading::{CREATE_NEW_PROCESS_GROUP, DETACHED_PROCESS};
 
 //TODO:
 // -- Implement profiles
@@ -12,8 +13,7 @@ fn main() {
     match redirector_dll_exists {
         Ok(_exists) => {
             if _exists == false {
-                println!("Redirector DLL not found!");
-                panic!();
+                panic!("Redirector DLL not found!");
             }
         },
 
@@ -55,5 +55,89 @@ fn main() {
         panic!("Game path not specified!");
     }
 
-    //TODO: Figure out a way to launch Fortnite
+    let server_dll = PathBuf::from("server.dll");
+    if is_server {
+        let server_dll_exists = std::fs::exists(server_dll.clone());
+        match server_dll_exists {
+            Ok(_exists) => {
+                if _exists == false {
+                    panic!("Server DLL not found!");
+                }
+            },
+
+            Err(e) => {
+                println!("Error! {}", e);
+                panic!();
+            }
+        }
+    }
+
+    if redirect_path.is_some() {
+        let redirect_path_buf = PathBuf::from((env!("LOCALAPPDATA").to_string() + "\\.yaflredirect").as_str());
+        let res = std::fs::write(redirect_path_buf, redirect_path.unwrap());
+        match res {
+            Ok(_) => {
+
+            },
+            Err(_) => {
+
+            },
+        }
+    }
+
+    let fortnite_binaries = Path::new(play_path.unwrap()).join("FortniteGame\\Binaries\\Win64");
+    let fortnite_launcher_path = fortnite_binaries.clone().as_path().join("FortniteLauncher.exe");
+    let fortnite_eac_path = fortnite_binaries.clone().as_path().join("FortniteClient-Win64-Shipping_EAC.exe");
+    let fortnite_client_path = fortnite_binaries.clone().as_path().join("FortniteClient-Win64-Shipping.exe");
+
+    let creation_flags: u32 = (DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP).0;
+
+    let launch_args: Vec<&str> = "-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -skippatchcheck -NOSSLPINNING -nobe -fromfl=eac -fltoken=3db3ba5dcbd2e16703f3978d -caldera=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiYmU5ZGE1YzJmYmVhNDQwN2IyZjQwZWJhYWQ4NTlhZDQiLCJnZW5lcmF0ZWQiOjE2Mzg3MTcyNzgsImNhbGRlcmFHdWlkIjoiMzgxMGI4NjMtMmE2NS00NDU3LTliNTgtNGRhYjNiNDgyYTg2IiwiYWNQcm92aWRlciI6IkVhc3lBbnRpQ2hlYXQiLCJub3RlcyI6IiIsImZhbGxiYWNrIjpmYWxzZX0.VAWQB67RTxhiWOxx7DBjnzDnXyyEnX7OljJm-j2d88G_WgwQ9wrE6lwMEHZHjBd1ISJdUO1UVUqkfLdU5nofBQ".split_whitespace().collect();
+
+    let mut launcher_process = Command::new(fortnite_launcher_path)
+        .current_dir(&fortnite_binaries)
+        .creation_flags(creation_flags)
+        .args(&launch_args)
+        .spawn()
+        .expect("Failed to launch FortniteLauncher!");
+    utils::nt_suspend_process(launcher_process.id());
+
+    let mut eac_process = match Command::new(fortnite_eac_path.to_str().unwrap())
+        .current_dir(&fortnite_binaries)
+        .creation_flags(creation_flags)
+        .args(&launch_args)
+        .spawn() {
+            Ok(result) => {
+                utils::nt_suspend_process(result.id());
+                result
+            },
+            Err(_) => {
+                let _ = launcher_process.kill();
+                panic!("Failed to start FortniteClient-Win64-Shipping_EAC!");
+            }
+        };
+
+    let mut fortnite_process = match Command::new(fortnite_client_path)
+        .current_dir(&fortnite_binaries)
+        .creation_flags(creation_flags)
+        .args(&launch_args)
+        .spawn() {
+            Ok(res) => {
+                res
+            },
+            Err(_) => {
+                let _ = launcher_process.kill();
+                let _ = eac_process.kill();
+                panic!("Failed to launch FortniteClient-Win64-Shipping!");
+            }
+        };
+
+    utils::inject_dll(fortnite_process.id(), PathBuf::from(
+        std::env::current_dir().unwrap()
+    ).join(redirector_dll_path.to_str().unwrap()).to_str().unwrap());
+
+    let _ = fortnite_process.wait();
+
+    let _ = launcher_process.kill();
+    let _ = eac_process.kill();
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,38 +1,105 @@
-use std::ffi::CString;
-use std::ptr::null_mut;
-use std::os::raw::c_void;
-use std::mem::transmute;
+use std::{ffi::CString, os::raw::c_void, ptr::null_mut};
 
-type HANDLE = *mut c_void;
-type NTSTATUS = i32;
+use windows::{Win32::{Foundation::{CloseHandle, HANDLE}, System::{Diagnostics::Debug::WriteProcessMemory, LibraryLoader::{GetModuleHandleA, GetProcAddress}, Memory::{MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_READWRITE, VirtualAllocEx, VirtualFreeEx}, Threading::{LPTHREAD_START_ROUTINE, OpenProcess, PROCESS_ALL_ACCESS, PROCESS_QUERY_INFORMATION, PROCESS_SUSPEND_RESUME}}}, core::PCSTR};
 
 #[link(name = "kernel32")]
 unsafe extern "system" {
-    fn LoadLibraryA(lpLibFileName: *const i8) -> HANDLE;
-    fn GetProcAddress(hModule: HANDLE, lpProcName: *const i8) -> *mut c_void;
-    fn OpenProcess(dwDesiredAccess: u32, bInheritHandle: i32, dwProcessId: u32) -> HANDLE;
-    fn CloseHandle(hObject: HANDLE) -> i32;
+    fn CreateRemoteThread(hProcess: HANDLE, lpThreadAttributes: *mut c_void, dwStackSize: usize, lpStartAddress: LPTHREAD_START_ROUTINE, lpParameter: *mut c_void, dwCreationFlags: u32, lpThreadId: *mut u32) -> HANDLE;
 }
 
-const PROCESS_SUSPEND_RESUME: u32 = 0x0800;
-const PROCESS_QUERY_INFORMATION: u32 = 0x0400;
+#[link(name = "ntdll")]
+unsafe extern "system" {
+    fn NtSuspendProcess(proc: HANDLE) -> i32;
+}
 
-fn nt_suspend_process(processHandle: HANDLE) -> i32 {
+pub fn nt_suspend_process(pid: u32) -> bool {
     unsafe {
-        let ntdll_name = CString::new("ntdll.dll").unwrap();
-        let ntdll = LoadLibraryA(ntdll_name.as_ptr());
-        if ntdll.is_null() {
-            panic!("Failed to load ntdll.dll");
+        let handle = OpenProcess(
+            PROCESS_SUSPEND_RESUME | PROCESS_QUERY_INFORMATION,
+            false,
+            pid
+        ).unwrap();
+
+        if handle.is_invalid() {
+            return false;
         }
 
-        let func_name = CString::new("NtSuspendProcess").unwrap();
-        let addr = GetProcAddress(ntdll, func_name.as_ptr());
-        if addr.is_null() {
-            panic!("NtSuspendProcess not found!");
-        }
+        let status = NtSuspendProcess(handle);
+        let _ = CloseHandle(handle);
 
-        let nt_suspend_process_func: extern "system" fn(HANDLE) -> NTSTATUS = transmute(addr);
-
-        nt_suspend_process_func(processHandle)
+        status == 0
     }
+}
+    
+// This is directly skidded from the original project
+pub fn inject_dll(pid: u32, dll_path: &str) -> bool {
+    let path_size = dll_path.len();
+
+    unsafe {
+        let handle = OpenProcess(
+            PROCESS_ALL_ACCESS, 
+            false, 
+            pid
+        ).unwrap();
+
+        if handle.is_invalid() {
+            return false;
+        }
+
+        let mem_loc = VirtualAllocEx(
+            handle, 
+            None, 
+            path_size, 
+            MEM_COMMIT | MEM_RESERVE, 
+            PAGE_READWRITE
+        );
+
+        let write_process_memory = WriteProcessMemory(
+            handle, 
+            mem_loc, 
+            CString::new(dll_path).unwrap().as_ptr() as *mut c_void, 
+            path_size, 
+            None
+        );
+        match write_process_memory {
+            Ok(_) => {},
+            Err(_) => {
+                return false;
+            }
+        }
+        
+        let kernel32_dll_name = CString::new("kernel32.dll").unwrap();
+        let kernel32_handle = GetModuleHandleA(PCSTR::from_raw(kernel32_dll_name.as_ptr() as *const u8));
+
+        let loadlibrary_name = CString::new("LoadLibraryA").unwrap();
+
+        let load_library_addr = GetProcAddress(kernel32_handle.unwrap(), PCSTR::from_raw(loadlibrary_name.as_ptr() as *const u8));
+        if load_library_addr.is_none() {
+            return false;
+        }
+        let load_library = load_library_addr.unwrap();
+
+        let load_library_fn: LPTHREAD_START_ROUTINE = std::mem::transmute(load_library);
+
+        let thread_handle = CreateRemoteThread(
+            handle, 
+            null_mut(), 
+            0, 
+            load_library_fn, 
+            mem_loc, 
+            0, 
+            null_mut()
+        );
+        if thread_handle.is_invalid() {
+            let _ = VirtualFreeEx(
+                handle, 
+                mem_loc, 
+                0, 
+                MEM_RELEASE
+            );
+            return false;
+        }
+    }
+
+    true
 }
